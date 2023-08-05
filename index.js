f0c9e2a38e303969325e3864b1b16be0ae700018
adcavalcant <aderc19@gmail.com>
2023-08-04 23:53:14 -0300

    Expressão regular para pegar tudo que está dentro dos parênteses ()

--- index.js
+++ index.js
@@ -16,26 +16,9 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-// //promises com .then
-// function pegaArquivo() {
-//   const encoding = "utf-8";
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch(trataErro);
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = "utf-8";
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if(erro){
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   });
-// }
-
 pegaArquivo("./arquivos/texto.md");
 pegaArquivo("./arquivos/texto.m");
 
-//\[[^[\]]*?\]
+// \[[^[\]]*?\]
+
+// \(https?:\/\/[^\s?#.].[^\s]*\)
