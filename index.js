e169bc59bb66756501ac0e92a3aa5f29ab082c9e
adcavalcant <aderc19@gmail.com>
2023-08-05 00:55:46 -0300

    Retornando os links corretamente

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import chalk from "chalk"; // module
 import fs from "fs";
 
-const textoTeste =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).";
-
-function extraiLinks(texto){
+function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-  const capturas = regex.exec(texto);
+  const capturas = [...texto.matchAll(regex)];
 
-  console.log(capturas);
+  const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
+  return resultados;
 }
-
-extraiLinks(textoTeste);
 
 function trataErro(erro) {
   console.log(erro);
@@ -22,13 +18,14 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const enconding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, enconding);
-    console.log(chalk.green(texto));
+    const mostraNoConsole = console.log;
+    mostraNoConsole(extraiLinks(texto));
   } catch (error) {
     trataErro(error);
   }
 }
 
-//pegaArquivo("./arquivos/texto.md");
+pegaArquivo("./arquivos/texto.md");
 //pegaArquivo("./arquivos/texto.m"); //->erro proposital para teste
 
 //usar regex101.com para testar
