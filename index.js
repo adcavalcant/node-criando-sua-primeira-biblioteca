d325bb6308d2a05ab6afbb517a889c4a1b43be11
adcavalcant <aderc19@gmail.com>
2023-08-05 00:36:24 -0300

    Capturando grupos (regex)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import chalk from "chalk"; // module
 import fs from "fs";
 
+const textoTeste =
+  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).";
+
+function extraiLinks(texto){
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = regex.exec(texto);
+
+  console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 function trataErro(erro) {
   console.log(erro);
   throw new Error(chalk.red(erro.code, `não há arquivo no diretório`));
@@ -16,9 +28,14 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-pegaArquivo("./arquivos/texto.md");
-pegaArquivo("./arquivos/texto.m");
+//pegaArquivo("./arquivos/texto.md");
+//pegaArquivo("./arquivos/texto.m"); //->erro proposital para teste
 
-// \[[^[\]]*?\]
+//usar regex101.com para testar
+// pega o que está dentro dos colchetes -> \[[^[\]]*?\]
 
-// \(https?:\/\/[^\s?#.].[^\s]*\)
+// pega o que está dentro dos parênteses -> \(https?:\/\/[^\s?#.].[^\s]*\)
+
+// pega tudo que precisamos -> \[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)
+
+// pega tudo que precisamos separadamente -> /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
