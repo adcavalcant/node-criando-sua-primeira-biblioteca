811e866b6d568895d75d38ace70797f0dadd02f0
adcavalcant <aderc19@gmail.com>
2023-08-08 14:04:46 -0300

    Tratando novos erros

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,24 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-  console.log(chalk.yellow("lista de links"), resultado);
+function imprimeLista(resultado, idenficador = '') {
+  console.log(
+    chalk.yellow("lista de links"), 
+    chalk.black.bgGreen(idenficador),
+    resultado
+    );
 }
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+  try {
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if(erro.code === 'ENOENT'){
+      console.log('arquivo ou diretório não existe');
+      return;
+    }
+  }
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(caminho);
     imprimeLista(resultado);
@@ -17,7 +29,7 @@ async function processaTexto(argumentos) {
     const arquivos = await fs.promises.readdir(caminho);
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-      imprimeLista(lista);
+      imprimeLista(lista, nomeDeArquivo);
     });
   }
 }
