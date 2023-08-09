ab68576f0318b9492e0c7b54856d303817b663e4
3S Cavalcante <aderc19@gmail.com>
2023-08-09 16:39:32 -0300

    validando os links e montando o objeto de retorno - finalizando a lib

--- src/cli.js
+++ src/cli.js
@@ -1,49 +1,48 @@
-import chalk from "chalk";
-import fs from "fs";
-import pegaArquivo from "./index.js";
-import { listaValidada } from "./http-validacao.js";
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArquivo from './index.js';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-function imprimeLista(valida, resultado, idenficador = "") {
+async function imprimeLista(valida, resultado, identificador = '') {
   if (valida) {
     console.log(
-      chalk.yellow("lista validada"),
-      chalk.black.bgGreen(idenficador),
-      listaValidada(resultado)
-    );
+      chalk.yellow('lista validada'),
+      chalk.black.bgGreen(identificador),
+      await listaValidada(resultado));    
   } else {
     console.log(
-      chalk.yellow("lista de links"),
-      chalk.black.bgGreen(idenficador),
-      resultado
-    );
+      chalk.yellow('lista de links'),
+      chalk.black.bgGreen(identificador),
+      resultado);
   }
 }
+
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
-  const valida = argumentos[3] === "--valida";
+  const valida = argumentos[3] === '--valida';
+
   try {
     fs.lstatSync(caminho);
   } catch (erro) {
-    if (erro.code === "ENOENT") {
-      console.log("arquivo ou diretório não existe");
+    if (erro.code === 'ENOENT') {
+      console.log('arquivo ou diretório não existe');
       return;
     }
   }
+
   if (fs.lstatSync(caminho).isFile()) {
-    const resultado = await pegaArquivo(caminho);
+    const resultado = await pegaArquivo(argumentos[2]);
     imprimeLista(valida, resultado);
   } else if (fs.lstatSync(caminho).isDirectory()) {
-    const arquivos = await fs.promises.readdir(caminho);
+    const arquivos = await fs.promises.readdir(caminho)
     arquivos.forEach(async (nomeDeArquivo) => {
-      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-      imprimeLista(valida, lista, nomeDeArquivo);
-    });
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+      imprimeLista(valida, lista, nomeDeArquivo)
+    })
   }
 }
-
-// rode -> node src/cli.js ./arquivos/texto.md
 
 processaTexto(caminho);
